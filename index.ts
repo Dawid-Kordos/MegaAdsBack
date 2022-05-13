6fde541924c3ae3f8c651c0f95439da85d0e8770
Dawid Kordos <dkordos82@wp.pl>
2022-05-13 21:08:48 +0200

    Error handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError, ValidationError} from './utils/errors';
 
 const app = express();
 
@@ -8,5 +9,13 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+
+//....
+
+app.get('/', async (req, res) => {
+    throw new ValidationError('WTF?');
+});
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => console.log('Server is running on port 3001...'));
