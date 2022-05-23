8cb5c588a787f7904cc64aa7c1841107a666243b
Dawid Kordos <dkordos82@wp.pl>
2022-05-23 22:22:54 +0200

    Moved DB connection data into config.

--- config/config.example.ts
+++ config/config.example.ts
@@ -1,6 +1,6 @@
 /*export const config = {
     dbHost: 'localhost',
-    user: 'user',
-    password: 'password',
-    database: 'database',
+    dbUser: 'user',
+    dbPassword: 'password',
+    dbDatabase: 'database',
 };*/
